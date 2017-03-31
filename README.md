ea0445f0600b6c89c4098c06f97277a1dcde8396
Mohamad Othman <mohamad.othman@actum.cz>
2017-03-31 18:12:03 +0200

    Changed Output Function. Added not modified header.

--- README.md
+++ README.md
@@ -10,13 +10,13 @@ The preferred way to install this extension is through [composer](http://getcomp
 Either run
 
 ```
-php composer.phar require --prefer-dist osmancode/thumbonthefly "*"
+php composer.phar require osmancode/thumbonthefly "*"
 ```
 
 or add
 
 ```
-"osmancode/thumbonthefly": "*"
+"osmancode/thumbonthefly": "dev-master"
 ```
 
 to the require section of your `composer.json` file.
--- src/Thumbonthefly.php
+++ src/Thumbonthefly.php
@@ -1,31 +1,25 @@
 <?php
 
-
-
 namespace Osmancode\Thumbonthefly;
-
-
 use Intervention\Image\Exception\InvalidArgumentException;
 use Intervention\Image\ImageManager;
 
-class ThumbsMaker
+class Thumbonthefly
 {
-
-    public $uploadsDir;
+    private static $uploadsDir;
     public $sizesWhiteList = ['70x70'];
     public $maxWidth = 2000;
     public $maxHeight = 2000;
-
-
     private $_w;
     private $_h;
     private $_img;
-
     private $_srcImg;
     private $_distImg;
 
-    public function initAndRun()
+    public static function init($uploadsDir)
     {
+        self::$uploadsDir = $uploadsDir;
+
         $uri = $_SERVER['REQUEST_URI'];
         $currentDir = dirname($_SERVER['SCRIPT_NAME']);
         $uri = ltrim($uri, $currentDir);
@@ -33,14 +27,13 @@ class ThumbsMaker
         preg_match('/(\d+)\/(\d+)\/(.*)?/', $uri, $matches);
 
         if (count($matches) == 4) {
-            $this->run($matches[1], $matches[2], $matches[3]);
+            new self($matches[1], $matches[2], $matches[3]);
         }
 
         throw new InvalidArgumentException();
     }
 
-
-    public function run($w, $h, $img)
+    private function __construct($w, $h, $img)
     {
         $this->_w = $w;
         $this->_h = $h;
@@ -50,7 +43,6 @@ class ThumbsMaker
         $distImg = $this->getDistImg();
 
         if ($this->getSizeOk()) {
-
             // thumb already exists
             if (is_file($distImg)) {
                 return $this->renderImage($distImg);
@@ -60,12 +52,10 @@ class ThumbsMaker
             $resource = $manager->make($srcImg)->fit($w, $h)->save($distImg);
 
             return $this->renderImage($resource->basePath());
-
         } else {
             return $this->renderImage($srcImg);
         }
     }
-
 
     private function getSizeOk()
     {
@@ -81,7 +71,7 @@ class ThumbsMaker
     private function getSrcImg()
     {
         if ($this->_srcImg === NULL) {
-            $this->_srcImg = "{$this->uploadsDir}/{$this->_img}";
+            $this->_srcImg = self::$uploadsDir . "/{$this->_img}";
 
             // src file not exists anymore
             if (!is_file($this->_srcImg)) {
@@ -101,15 +91,39 @@ class ThumbsMaker
             $hash = hash('md4', implode('#', $hashComponents));
             $distFileName = "{$hash}." . pathinfo($srcImg, PATHINFO_EXTENSION);
 
-            $this->_distImg = "{$this->uploadsDir}/.thumbnails/{$distFileName}";
+            $this->_distImg = self::$uploadsDir . "/.thumbnails/{$distFileName}";
         }
         return $this->_distImg;
     }
 
     private function renderImage($img)
     {
-//        return \Yii::$app->response->xSendFile($img, $this->_img, ['inline' => true,]);
-        return \Yii::$app->response->sendFile($img, $this->_img, ['inline' => true,]);
-    }
+        $imgModificationTime = filemtime($img);
 
+        $imgModificationTimeAsString = date('D, d M Y H:i:s ', $imgModificationTime) . 'GMT';
+        $etag = hash('md4', $imgModificationTime);
+
+        $ifModifiedSince = isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) ? strtotime($_SERVER['HTTP_IF_MODIFIED_SINCE']) : false;
+
+        $ifNoneMatch = isset($_SERVER['HTTP_IF_NONE_MATCH']) ? $_SERVER['HTTP_IF_NONE_MATCH'] : false;
+
+        if (
+            (($ifNoneMatch && $ifNoneMatch == $etag) || (!$ifNoneMatch)) &&
+            ($ifModifiedSince !== false && $ifModifiedSince == $imgModificationTime)
+        ) {
+            header('HTTP/1.1 304 Not Modified');
+            exit();
+        } else {
+            header('Last-Modified: ' . gmdate('D, d M Y H:i:s \G\M\T', $imgModificationTime));
+            header("ETag: $etag");
+
+            $imgInfo = getimagesize($img);
+
+
+            header("Content-type: {$imgInfo['mime']}");
+            header('Content-Length: ' . filesize($img));
+            readfile($img);
+            exit();
+        }
+    }
 }
